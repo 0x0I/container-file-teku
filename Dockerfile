10368134dd446d444e0a564ade66972d190b41a2
Ahmad Wilson <zer0ne.io.x@example.com>
2021-10-21 17:34:53 -0400

    initial setup of teku container infra

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,68 @@
-ARG base_version="ubuntu:21.04"
+ARG build_version="openjdk:17.0.1-slim-buster"
+ARG teku_version=21.10.1
+ARG build_type="source"
 
-# ******* Stage: base ******* #
-FROM ${base_version} as base
+# ******* Stage: builder ******* #
+FROM ${build_version} as builder-source
+
+ARG teku_version
+
+RUN apt update && apt install --yes --no-install-recommends git
+
+WORKDIR /tmp
+RUN git clone --depth 1 --branch ${teku_version} https://github.com/Consensys/teku.git
+
+RUN cd teku && ./gradlew distTar installDist
+RUN mkdir /install && cp -r /tmp/teku/build/install/teku/* /install
+
+FROM ${build_version} as builder-package
+
+ARG teku_version
+
+RUN apt update && apt install --yes --no-install-recommends curl ca-certificates
+
+RUN mkdir /install && curl -L https://artifacts.consensys.net/public/teku/raw/names/teku.tar.gz/versions/${teku_version}/teku-${teku_version}.tar.gz -o teku.tar.gz && \
+		tar xzf teku.tar.gz -C /install --strip-components 1
+
+FROM builder-${build_type} as build-condition
+
+FROM ${build_version} as base
+
+COPY --from=build-condition /install /usr/local/teku
 
 RUN apt update && apt install --yes --no-install-recommends \
-    apache2 \
     ca-certificates \
     curl \
+    python3-pip \
     tini \
-    && rm -rf /var/lib/apt/lists/*
+    # apt cleanup
+	&& apt-get autoremove -y; \
+	apt-get clean; \
+	update-ca-certificates; \
+	rm -rf /tmp/* /var/tmp/* /var/lib/apt/lists/*
 
 WORKDIR /docker-entrypoint.d
 COPY entrypoints /docker-entrypoint.d
-COPY scripts/entrypoint.sh /usr/local/bin/demo-entrypoint
+COPY scripts/entrypoint.sh /usr/local/bin/teku-entrypoint
 
-WORKDIR /scripts
-COPY scripts /scripts
+COPY scripts/teku-helper.py /usr/local/bin/teku-helper
+RUN chmod 775 /usr/local/bin/teku-helper
 
-ENV DEMO_USER=world
+RUN pip3 install click requests pyaml
 
-ENTRYPOINT ["demo-entrypoint"]
+ENTRYPOINT ["teku-entrypoint"]
 
 # ******* Stage: testing ******* #
 FROM base as test
+
+ENV PATH="/usr/local/teku/bin:${PATH}"
 
 ARG goss_version=v0.3.16
 
 RUN curl -fsSL https://goss.rocks/install | GOSS_VER=${goss_version} GOSS_DST=/usr/local/bin sh
 
 WORKDIR /test
+
 COPY test /test
 
 CMD ["goss", "--gossfile", "/test/goss.yaml", "validate"]
@@ -36,8 +70,20 @@ CMD ["goss", "--gossfile", "/test/goss.yaml", "validate"]
 # ******* Stage: release ******* #
 FROM base as release
 
-#      web
-#       ↓
-EXPOSE 80
+ARG version=0.1.0
 
-CMD ["curl", "demo.01labs.net"]
+ENV PATH="/usr/local/teku/bin:${PATH}"
+
+LABEL 01labs.image.authors="zer0ne.io.x@gmail.com" \
+	01labs.image.vendor="O1 Labs" \
+	01labs.image.title="0labs/teku" \
+	01labs.image.description="Open-source Ethereum 2.0 client written in Java" \
+	01labs.image.source="https://github.com/0x0I/container-file-teku/blob/${version}/Dockerfile" \
+	01labs.image.documentation="https://github.com/0x0I/container-file-teku/blob/${version}/README.md" \
+	01labs.image.version="${version}"
+
+#       p2p/tcp   p2p/udp   api   metrics
+#          ↓         ↓       ↓      ↓
+EXPOSE 9000/tcp   9000/udp  5051   8008
+
+CMD ["teku"]
---
+++ scripts/teku-helper.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+import json
+import os
+import subprocess
+import sys
+
+import click
+import requests
+import urllib.request
+import yaml
+
+@click.group()
+@click.option('--debug/--no-debug', default=False)
+def cli(debug):
+    pass
+
+@cli.group()
+def config():
+    pass
+
+@cli.group()
+def status():
+    pass
+
+###
+# Commands for application configuration customization and inspection
+###
+
+DEFAULT_TEKU_CONFIG_PATH = "/etc/teku/config.yml"
+DEFAULT_TEKU_DATADIR = "/root/.eth2"
+
+DEFAULT_API_HOST_ADDR = 'http://localhost:5051'
+DEFAULT_API_METHOD = 'GET'
+DEFAULT_API_PATH = 'eth/v1/node/identity'
+DEFAULT_API_DATA = '{}'
+
+
+def print_json(json_blob):
+    print(json.dumps(json_blob, indent=4, sort_keys=True))
+
+def execute_command(command):
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+    if process.returncode > 0:
+        print('Executing command \"%s\" returned a non-zero status code %d' % (command, process.returncode))
+        sys.exit(process.returncode)
+
+    if error:
+        print(error.decode('utf-8'))
+
+    return output.decode('utf-8')
+
+@config.command()
+@click.option('--config-path',
+              default=lambda: os.environ.get("TEKU_CONFIG", DEFAULT_TEKU_CONFIG_PATH),
+              show_default=DEFAULT_TEKU_CONFIG_PATH,
+              help='path to teku configuration file to generate or customize from environment config settings')
+def customize(config_path):
+    config_dict = dict()
+    if os.path.isfile(config_path):
+        with open(config_path, "r") as stream:
+            try:
+                config_dict = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+    for var in os.environ.keys():
+        var_split = var.split('_')
+        if len(var_split) == 2 and var_split[0].lower() == "config":
+            config_setting = var_split[1]
+            value = os.environ[var]
+
+            # ensure values are cast appropriately
+            if value.isdigit():
+                value = int(value)
+            elif value.lower() == "true":
+                value = True
+            elif value.lower() == "false":
+                value = False
+            config_dict[config_setting] = value
+
+    with open(config_path, 'w+') as f:
+        yaml.dump(config_dict, f)
+
+@status.command()
+@click.option('--host-addr',
+              default=lambda: os.environ.get("API_HOST_ADDR", DEFAULT_API_HOST_ADDR),
+              show_default=DEFAULT_API_HOST_ADDR,
+              help='Teku REST API host address in format <protocol(http/https)>://<IP>:<port>')
+@click.option('--api-method',
+              default=lambda: os.environ.get("API_METHOD", DEFAULT_API_METHOD),
+              show_default=DEFAULT_API_METHOD,
+              help='HTTP method to execute a part of request')
+@click.option('--api-path',
+              default=lambda: os.environ.get("API_PATH", DEFAULT_API_PATH),
+              show_default=DEFAULT_API_PATH,
+              help='Restful API path to target resource')
+@click.option('--api-data',
+              default=lambda: os.environ.get("API_DATA", DEFAULT_API_DATA),
+              show_default=DEFAULT_API_DATA,
+              help='Restful API request body data included within POST requests')
+def api_request(host_addr, api_method, api_path, api_data):
+    """
+    Execute RESTful API HTTP request
+    """
+
+    try:
+        if api_method.upper() == "POST":
+            resp = requests.post(
+                "{host}/{path}".format(host=host_addr, path=api_path),
+                json=json.loads(api_data),
+                headers={'Content-Type': 'application/json'})
+        else:
+            resp = requests.get("{host}/{path}".format(host=host_addr, path=api_path))
+
+        # signal error if non-OK response status
+        resp.raise_for_status()
+
+        print_json(resp.json())
+    except requests.exceptions.RequestException as err:
+        sys.exit(print_json({
+            "error": "API request to {host} failed with: {error}".format(
+                host=host_addr,
+                error=err
+            )
+        }))
+    except json.decoder.JSONDecodeError:
+        print(resp.text)
+
+
+if __name__ == "__main__":
+    cli()
